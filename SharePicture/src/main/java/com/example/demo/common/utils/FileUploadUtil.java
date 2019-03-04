7aa368def91efbb7d073ee0b732097b0448e5e21
Administrator <Administrator@192.168.43.115>
2019-03-05 01:00:17 +0800

    个人中心前端

--- SharePicture/src/main/java/com/example/demo/common/utils/FileUploadUtil.java
+++ SharePicture/src/main/java/com/example/demo/common/utils/FileUploadUtil.java
@@ -1,7 +1,38 @@
 package com.example.demo.common.utils;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 上传图片工具
+ * @author Administrator
+ *
+ */
 public class FileUploadUtil
-{
-	
+{	
+	/**
+	 * 判断文件是否为图片
+	 * @param file
+	 * @return
+	 */
+	public static boolean isImage(MultipartFile file)
+	{
+		try
+		{
+			Image image=ImageIO.read(file.getInputStream());
+			if(image!=null&&image.getWidth(null)>0&&image.getHeight(null)>0) 
+			{
+				return true;
+			}
+		} 
+		catch (IOException e)
+		{
+			return false;
+		}
+		return false;
+	}
 
 }
---
+++ SharePicture/src/test/java/com/example/demo/UploadPictureTest.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import java.io.File;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.common.utils.FileUploadUtil;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UploadPictureTest
+{	
+	@Test
+	public void isImageTest()
+	{
+//		File file = new File("C:/Users/Administrator/Desktop/新建文本文档 (2).png");
+//		System.out.println(FileUploadUtil.isImage(file));
+
+	}
+
+}
